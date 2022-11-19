33a3236fdb694363454f10981791ca70c98221d0
janek <jan.wielgosinski@th-ey.com>
2022-11-19 15:11:50 +0100

    refactor of logic

--- src/Logic.js
+++ src/Logic.js
@@ -1,3 +1,30 @@
+const gcd = (a, b) => a ? gcd(b % a, a) : b;
+const lcm = (a, b) => a * b / gcd(a, b);
+
+// hyperperiod
+// liczy hyperperiod dla obecnej listy zadan.
+const calcHyperPeriod = (zadania) => {
+  return zadania.map(zadanie => zadanie.okres).reduce(lcm);
+}
+
+// zwraca index zadania ktore ma priorytet w obecej jednosce czasu
+const estimatePriority = (zadaniaCzasuRzeczywistego, zadania) => {
+  // hyperperiod tu powinnen byc ze zmiennej wyciagany ale react sie nie slucha.
+  let tempPeriod = calcHyperPeriod(zadania)
+  let p = -1
+
+  zadaniaCzasuRzeczywistego.forEach(({ okres, czasWykonania }, i) => {
+    if (czasWykonania !== 0) {
+      if (tempPeriod > okres) {
+        tempPeriod = okres
+        p = i
+      }
+    }
+  })
+
+  return p
+}
+
 export const warunkiSzeregowalnosci = (zadania) => {
   // obliczanie U
   let czy_jest_szeregowalny = false;
@@ -25,66 +52,31 @@ export const warunkiSzeregowalnosci = (zadania) => {
 
 export const uszereguj = (zadania) => {
   let zadaniaCzasuRzeczywistego = JSON.parse(JSON.stringify(zadania));
-
   const timeline = []; // {jednostkaCzasu: number, nazwaZadania: string}
-  [...Array(calcHyperPeriod(zadania)).keys()].forEach((t) => {
-    const indexZadania = estimatePriority(zadaniaCzasuRzeczywistego);
+  [...Array(calcHyperPeriod(zadania)).keys()].forEach(t => {
+    const indexZadania = estimatePriority(zadaniaCzasuRzeczywistego, zadania);
 
     if (indexZadania !== -1) {
-      // procesor jest zajęty wykonywaniem zadania
-      console.log(`t${t} -->t${t + 1}: TASK ${zadania[indexZadania].nazwa}`);
-      timeline.push({
-        jednostkaCzasu: t,
-        nazwaZadania: zadania[indexZadania].nazwa,
-      });
-      zadaniaCzasuRzeczywistego[indexZadania].czasWykonania -= 1;
+      // wykonuje sie task
+      console.log(`t${t} -->t${t+1}: TASK ${zadania[indexZadania].nazwa}`);
+      timeline.push({jednostkaCzasu: t, nazwaZadania: zadania[indexZadania].nazwa})
+      zadaniaCzasuRzeczywistego[indexZadania].czasWykonania -= 1
     } else {
-      // Procesor nie wykonuje żadnego zadania
-      timeline.push({ jednostkaCzasu: t, nazwaZadania: "IDLE" });
-      console.log(`t${t} -->t${t + 1}: IDLE`);
+      // nic sie nie dzieje
+      timeline.push({jednostkaCzasu: t, nazwaZadania: 'IDLE'})
+      console.log(`t${t} -->t${t+1}: IDLE`);
     }
 
-    // uaktualnij okresu po kazdym cyklu procesora
-    zadaniaCzasuRzeczywistego = zadaniaCzasuRzeczywistego.map(
-      (zadanieCzasuRzeczywistego) => {
-        zadanieCzasuRzeczywistego.okres -= 1;
-        if (zadanieCzasuRzeczywistego.okres === 0) {
-          let index = zadania.findIndex(
-            (it) => zadanieCzasuRzeczywistego.nazwa === it.nazwa
-          );
-          zadanieCzasuRzeczywistego = JSON.parse(
-            JSON.stringify(zadania[index])
-          );
-        }
-        return zadanieCzasuRzeczywistego;
+    // Update Period after each clock cycle
+    zadaniaCzasuRzeczywistego = zadaniaCzasuRzeczywistego.map(zadanieCzasuRzeczywistego => {
+      zadanieCzasuRzeczywistego.okres -= 1;
+      if (zadanieCzasuRzeczywistego.okres === 0) {
+        let index = zadania.findIndex(it => zadanieCzasuRzeczywistego.nazwa === it.nazwa);
+        zadanieCzasuRzeczywistego = JSON.parse(JSON.stringify(zadania[index]));
       }
-    );
-  });
+      return zadanieCzasuRzeczywistego;
+    })
+  })
 
   return timeline;
-};
-
-const calcHyperPeriod = (zadania) => {
-  return zadania.map((zadanie) => zadanie.okres).reduce(lcm);
-};
-
-const gcd = (a, b) => (a ? gcd(b % a, a) : b);
-
-const lcm = (a, b) => (a * b) / gcd(a, b);
-
-// zwraca index zadania ktore ma priorytet w obecej jednosce czasu
-const estimatePriority = (zadaniaCzasuRzeczywistego) => {
-  let tempPeriod = calcHyperPeriod(zadaniaCzasuRzeczywistego);
-  let p = -1;
-
-  zadaniaCzasuRzeczywistego.forEach(({ okres, czasWykonania }, i) => {
-    if (czasWykonania !== 0) {
-      if (tempPeriod > okres) {
-        tempPeriod = okres;
-        p = i;
-      }
-    }
-  });
-
-  return p;
 };
