329788dd38fc0f95c093899535de00e15536f01e
janek <jan.wielgosinski@th-ey.com>
2022-11-19 14:01:54 +0100

    refactor of logic

--- src/Logic.js
+++ src/Logic.js
@@ -1,103 +1,85 @@
-import {useEffect, useState} from "react";
+export const warunkiSzeregowalnosci = (zadania) => {
+    // obliczanie U
+    let czy_jest_szeregowalny = false;
+    const U = zadania
+        .map(zadanie => Math.floor(zadanie.czasWykonania / zadanie.okres))
+        .reduce((suma, i) => suma + i, 0);
 
-
-const Logic = () => {
-    const [zadania, setZadania] = useState([
-        {nazwa: 'P1', okres: 5, czasWykonania: 2},
-        {nazwa: 'P2', okres: 10, czasWykonania: 2},
-        {nazwa: 'P3', okres: 20, czasWykonania: 3},
-    ]); // {nazwa: string, priorytet: number, okres: number, czasWykonania: number}
-    const [hyperPeriod, setHyperPeriod] = useState(undefined);
-    const [U, setU] = useState(undefined);
-    const [uUtil, setUUtil] = useState(undefined);
-
-    useEffect(() => {
-        uszereguj();
-    },[]);
-
-    // sprawdzenie czy dany zestaw zadan da sie zaszeregować
-    const warunkiSzeregowalnosci = () => {
-        // obliczanie U
-        const U = zadania
-            .map(zadanie => Math.floor(zadanie.czasWykonania / zadanie.okres))
-            .reduce((suma, i) => suma + i, 0);
-
-        setU(U);
-
-        if (U <= 1) {
-            // Twierdzenie o kresie algorytmu
-            const sched_util = zadania.length*(Math.pow(2, 1/zadania.length)-1);
-            setUUtil(sched_util);
-            if (U <= sched_util) {
-                console.log("Mozna zaszeregowac RMS")
-            } else {
-                console.log("Nie mozna zaszeregowac RMS")
-            }
+    let warunek_dostateczny;
+    if (U <= 1) {
+        // Twierdzenie o kresie algorytmu
+        warunek_dostateczny = zadania.length*(Math.pow(2, 1/zadania.length)-1);
+        if (U <= warunek_dostateczny) {
+            czy_jest_szeregowalny = true;
+            console.log("Mozna zaszeregowac RMS")
         } else {
-            console.log("U > 1!");
+            console.log("Nie mozna zaszeregowac RMS")
         }
+    } else {
+        console.log("U > 1!");
     }
 
-
-    const gcd = (a, b) => a ? gcd(b % a, a) : b;
-    const lcm = (a, b) => a * b / gcd(a, b);
-
-    // hyperperiod
-    // liczy hyperperiod dla obecnej listy zadan.
-    const calcHyperPeriod = () => {
-        return zadania.map(zadanie => zadanie.okres).reduce(lcm);
-    }
-
-    // zwraca index zadania ktore ma priorytet w obecej jednosce czasu
-    const estimatePriority = (zadaniaCzasuRzeczywistego) => {
-        // hyperperiod tu powinnen byc ze zmiennej wyciagany ale react sie nie slucha.
-        let tempPeriod = calcHyperPeriod()
-        let p = -1
-
-        zadaniaCzasuRzeczywistego.forEach(({ okres, czasWykonania }, i) => {
-            if (czasWykonania !== 0) {
-                if (tempPeriod > okres) {
-                    tempPeriod = okres
-                    p = i
-                }
-            }
-        })
-
-        return p
-    }
-
-    const uszereguj = () => {
-        let zadaniaCzasuRzeczywistego = JSON.parse(JSON.stringify(zadania));
-        const timeline = []; // {jednostkaCzasu: number, nazwaZadania: string}
-        [...Array(calcHyperPeriod()).keys()].forEach(t => {
-
-            const indexZadania = estimatePriority(zadaniaCzasuRzeczywistego);
-
-            if (indexZadania !== -1) {
-                // wykonuje sie task
-                console.log(`t${t} -->t${t+1}: TASK ${zadania[indexZadania].nazwa}`);
-                timeline.push({jednostkaCzasu: t, nazwaZadania: zadania[indexZadania].nazwa})
-                zadaniaCzasuRzeczywistego[indexZadania].czasWykonania -= 1
-            } else {
-                // nic sie nie dzieje
-                timeline.push({jednostkaCzasu: t, nazwaZadania: 'IDLE'})
-                console.log(`t${t} -->t${t+1}: IDLE`);
-            }
-
-            // Update Period after each clock cycle
-            zadaniaCzasuRzeczywistego = zadaniaCzasuRzeczywistego.map(zadanieCzasuRzeczywistego => {
-                zadanieCzasuRzeczywistego.okres -= 1;
-                if (zadanieCzasuRzeczywistego.okres === 0) {
-                    let index = zadania.findIndex(it => zadanieCzasuRzeczywistego.nazwa === it.nazwa);
-                    zadanieCzasuRzeczywistego = JSON.parse(JSON.stringify(zadania[index]));
-                }
-                return zadanieCzasuRzeczywistego;
-            })
-        })
-        console.log(timeline);
-    }
-
-    return (<div>haha</div>)
+    return {U, warunek_dostateczny, czy_jest_szeregowalny}
 }
 
-export default Logic;
+export const uszereguj = (zadania) => {
+    let zadaniaCzasuRzeczywistego = JSON.parse(JSON.stringify(zadania));
+
+    const timeline = []; // {jednostkaCzasu: number, nazwaZadania: string}
+    [...Array(calcHyperPeriod()).keys()].forEach(t => {
+
+        const indexZadania = estimatePriority(zadaniaCzasuRzeczywistego);
+
+        if (indexZadania !== -1) {
+            // procesor jest zajęty wykonywaniem zadania
+            console.log(`t${t} -->t${t+1}: TASK ${zadania[indexZadania].nazwa}`);
+            timeline.push({jednostkaCzasu: t, nazwaZadania: zadania[indexZadania].nazwa})
+            zadaniaCzasuRzeczywistego[indexZadania].czasWykonania -= 1
+        } else {
+            // Procesor nie wykonuje żadnego zadania
+            timeline.push({jednostkaCzasu: t, nazwaZadania: 'IDLE'})
+            console.log(`t${t} -->t${t+1}: IDLE`);
+        }
+
+        // uaktualnij okresu po kazdym cyklu procesora
+        zadaniaCzasuRzeczywistego = zadaniaCzasuRzeczywistego.map(zadanieCzasuRzeczywistego => {
+            zadanieCzasuRzeczywistego.okres -= 1;
+            if (zadanieCzasuRzeczywistego.okres === 0) {
+                let index = zadania.findIndex(it => zadanieCzasuRzeczywistego.nazwa === it.nazwa);
+                zadanieCzasuRzeczywistego = JSON.parse(JSON.stringify(zadania[index]));
+            }
+            return zadanieCzasuRzeczywistego;
+        })
+    })
+
+    return timeline;
+}
+
+const calcHyperPeriod = (zadania) => {
+    return zadania.map(zadanie => zadanie.okres).reduce(lcm);
+}
+
+const gcd = (a, b) => a ? gcd(b % a, a) : b;
+
+const lcm = (a, b) => a * b / gcd(a, b);
+
+// zwraca index zadania ktore ma priorytet w obecej jednosce czasu
+const estimatePriority = (zadaniaCzasuRzeczywistego) => {
+
+    let tempPeriod = calcHyperPeriod()
+    let p = -1
+
+    zadaniaCzasuRzeczywistego.forEach(({ okres, czasWykonania }, i) => {
+        if (czasWykonania !== 0) {
+            if (tempPeriod > okres) {
+                tempPeriod = okres
+                p = i
+            }
+        }
+    })
+
+    return p
+}
+
+
+
